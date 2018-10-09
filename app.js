c11a11ce903be0f50edc74fb615c33a4aecff612
Jared York <jared.york@jaredyork.com>
2018-10-09 10:33:35 -0400

    made changes.

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var io = require('socket.io').listen(server);
 var port = 8080;
 server.listen(port);
 
+// File System API
+var fs = require('fs');
+
 console.log("Server started up...");
 
 app.use(express.static('public'));
@@ -26,5 +29,35 @@ io.sockets.on('connection', function(socket) {
 
     socket.on('client-connect', function() {
         console.log("Browser window loaded for client.");
+
+        var input = fs.createReadStream('keywords.txt');
+        readKeywordFile(socket, input, printKeywordData);
     });
 });
+
+function readKeywordFile(socket, input, printKeywordData) {
+    var remaining = '';
+
+    input.on('data', function(data) {
+        remaining += data;
+
+        var index = remaining.indexOf('\n');
+        while (index > -1) {
+            var line = remaining.substring(0, index);
+            remaining = remaining.substring(index + 1);
+            printKeywordData(socket, line);
+            index = remaining.indexOf('\n');
+        }
+    });
+
+    input.on('end', function() {
+        if (remaining.length > 0) {
+            printKeywordData(socket, remaining);
+        }
+    });
+}
+
+function printKeywordData(socket, data) {
+    console.log('Line: ' + data);
+    socket.emit('send-keywords', data)
+}

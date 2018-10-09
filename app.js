7b65c5ff8fe5eda52ba28f2fc693c61a629dff82
Jared York <jared.york@jaredyork.com>
2018-10-08 21:19:13 -0400

    made changes

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 var http = require('http');
+var io = require('socket.io')(http);
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
 }).listen(8080, 'localhost');
+
+io.on('connection', function(socket) {
+    socket.on('connect', function() {
+        console.log('A new user connected!');
+    });
+});
+
+
 console.log('Server running at http://localhost:8080/')

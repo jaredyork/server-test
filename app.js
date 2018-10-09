fa3b0f7a3aaad766048c6b236d78eea1db189b90
Jared York <jared.york@jaredyork.com>
2018-10-08 21:57:55 -0400

    made changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
-var port = 8000;
+var port = 8080;
 server.listen(port);
 
 console.log("Server started up...");

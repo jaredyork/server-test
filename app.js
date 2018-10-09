9780c78ffa5d0f163105133f7b992362eb20da1c
Jared York <jared.york@jaredyork.com>
2018-10-08 20:59:36 -0400

    made changes

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
-var app = require("express");
-var http = require("http").Server(app);
-var io = require("socket.io")(http);
-
-io.on('connection', function(socket) {
-    console.log("A user connected!");
-
-    socket.on("client-connect", function() {
-        console.log("A client connected, woohoo!");
-    });
-});
-
-http.listen(8080, function() {
-    console.log('Listening on *:8080');
-});
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(8080, 'localhost');
+console.log('Server running at http://localhost:8080/')
